06c89fce97f477aaedec4ea5170748a669c0e343
Brian Faust <hello@basecode.sh>
2020-11-04 04:58:32 +0200

    register blade component

---
+++ src/Providers/DataBagsServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Laravel Data Bags.
+ *
+ * (c) Konceiver Oy <info@konceiver.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Konceiver\Skeleton\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+use Konceiver\DataBags\View\Components\DataBag;
+
+class DataBagsServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Blade::component('data-bag', DataBag::class);
+    }
+}
