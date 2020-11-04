c399af1f2e86d665e31e07bf89f5602a66c74f00
Brian Faust <hello@basecode.sh>
2020-11-04 04:52:13 +0200

    implement ResolverFactory

---
+++ src/ResolverFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Laravel Data Bags.
+ *
+ * (c) Konceiver Oy <info@konceiver.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Konceiver\DataBags;
+
+use InvalidArgumentException;
+
+final class ResolverFactory
+{
+    /**
+     * @return mixed
+     */
+    public static function make(string $resolver, string $key)
+    {
+        if ($resolver === 'controller') {
+            return DataBag::resolveByController($key);
+        }
+
+        if ($resolver === 'domain') {
+            return DataBag::resolveByDomain($key);
+        }
+
+        if ($resolver === 'name') {
+            return DataBag::resolveByName($key);
+        }
+
+        if ($resolver === 'path') {
+            return DataBag::resolveByPath($key);
+        }
+
+        throw new InvalidArgumentException("Failed to find a resolver for [$resolver]");
+    }
+}
---
+++ src/View/Components/DataBag.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Laravel Data Bags.
+ *
+ * (c) Konceiver Oy <info@konceiver.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Konceiver\DataBags\View\Components;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\View\Component;
+use Konceiver\DataBags\ResolverFactory;
+
+final class DataBag extends Component
+{
+    private string $key;
+
+    private string $resolver;
+
+    private string $view;
+
+    public function __construct(string $key, string $resolver, string $view)
+    {
+        $this->key      = $key;
+        $this->resolver = $resolver;
+        $this->view     = $view;
+    }
+
+    public function render(): \Illuminate\View\View
+    {
+        return View::make($this->view, ResolverFactory::make($this->resolver, $this->key));
+    }
+}
