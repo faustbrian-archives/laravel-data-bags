f734628e83f45a8d8d7380cf6b7a4a2b898bf37e
Brian Faust <faustbrian@users.noreply.github.com>
2020-11-04 05:01:56 +0200

    Update DataBagsServiceProvider.php

--- src/Providers/DataBagsServiceProvider.php
+++ src/Providers/DataBagsServiceProvider.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Konceiver\Skeleton\Providers;
+namespace Konceiver\DataBags\Providers;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
